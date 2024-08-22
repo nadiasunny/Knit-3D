ce385e563d8b419ef6244a359a8d3621af3be2e5
Nadia Hina <nadianadia062@gmail.com>
2024-08-21 20:46:32 -0700

    added elements to html page

--- knitCal.js
+++ knitCal.js
@@ -1,12 +1,21 @@
 //gauge = 4 in by 4 in
-let rowsToAchieveWidth = document.getElementById('..');
-let rowstoAchiceLength = document.getElementById('[[');
 
-let currentLength = document.getElementById('ll');
-let desiredLength = document.getElementById('pp');
-let numOfRows = (currentLength*rowstoAchiceLength)/4;
-let finalNumOfRows = (desiredLength*rowstoAchiceLength)/4;
+document.getElementById('toDesired').onsubmit = calculateFinalWL
 
-let rowsToDesiredLength = finalNumOfRows - numOfRows
 
-//let finalWidth = 
+function calculateFinalWL(event){
+    event.preventDefault();
+    
+    let rowsToAchieveWidth = document.getElementById('gaugeWid');
+    let rowstoAchiceLength = document.getElementById('gaugeLen');
+
+    let currentLength = document.getElementById('currentLen');
+    let desiredLength = document.getElementById('desiredLen');
+    let numOfRows = (Num(currentLength)*Num(rowstoAchiceLength))/4;
+    let finalNumOfRows = (Num(desiredLength)*Num(rowstoAchiceLength))/4;
+
+    let rowsToDesiredLength = finalNumOfRows - numOfRows;
+    console.log(rowsToDesiredLength);
+    document.getElementById('rowsLeft').value = rowsToDesiredLength;
+    //let finalWidth = 
+}
